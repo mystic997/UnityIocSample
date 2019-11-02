461e42c8b4a40ac8f90d62b0fa8cee4b85b6e4a6
Krisztián Pörzsölt <pkrisztian997@gmail.com>
2019-11-02 21:56:10 +0100

    Add basic CustomControls

---
+++ UnityIoCWpfSample/UnityIoCWpfSample/UI/Views/TreeListViewTable/CustomControls/TreeListView.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace UnityIoCWpfSample.UI.Views.TreeListViewTable.CustomControls
+{
+    public class TreeListView : TreeView
+    {
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new TreeListViewItem();
+        }
+
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            return item is TreeListViewItem;
+        }
+    }
+}
---
+++ UnityIoCWpfSample/UnityIoCWpfSample/UI/Views/TreeListViewTable/CustomControls/TreeListViewItem.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace UnityIoCWpfSample.UI.Views.TreeListViewTable.CustomControls
+{
+    public class TreeListViewItem : TreeViewItem
+    {
+        private int _level = -1;
+
+        public int Level
+        {
+            get
+            {
+                if (_level == -1)
+                {
+                    TreeListViewItem parent =
+                        ItemsControl.ItemsControlFromItemContainer(this)
+                            as TreeListViewItem;
+                    _level = (parent != null) ? parent.Level + 1 : 0;
+                }
+                return _level;
+            }
+        }
+
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new TreeListViewItem();
+        }
+
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            return item is TreeListViewItem;
+        }
+    }
+}
