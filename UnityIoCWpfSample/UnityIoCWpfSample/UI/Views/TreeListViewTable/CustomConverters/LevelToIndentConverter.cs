55a7870338295b8f7a538e22628fe4662d49c591
Krisztián Pörzsölt <pkrisztian997@gmail.com>
2019-11-02 22:11:00 +0100

    Add item level to indent converter

---
+++ UnityIoCWpfSample/UnityIoCWpfSample/UI/Views/TreeListViewTable/CustomConverters/LevelToIndentConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace UnityIoCWpfSample.UI.Views.TreeListViewTable.CustomConverters
+{
+    public class LevelToIndentConverter : IValueConverter
+    {
+        private const double INDENT_SIZE = 8.0;
+
+        public object Convert(object o, Type type, object parameter, CultureInfo culture)
+        {
+            return new Thickness((int)o * INDENT_SIZE, 0, 0, 0);
+        }
+
+        public object ConvertBack(object o, Type type, object parameter, CultureInfo culture)
+        {
+            return new Thickness((int)o / INDENT_SIZE, 0, 0, 0);
+        }
+    }
+}
